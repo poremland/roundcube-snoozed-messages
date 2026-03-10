38953996b7d80505395b3f6cf2f76c27381968cf
Paul Oremland <paul@oremland.net>
2026-03-09 17:39:53 -0700

    fix(conductor): Apply review suggestions for track 'Fix UI bug in Safari where snoozed labels bunch at the bottom of the screen.'

--- snoozed_messages.php
+++ snoozed_messages.php
@@ -18,7 +18,7 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  *
- * @version 1.1.0
+ * @version 1.1.1
  * @author Paul Oremland
  * @license AGPL-3.0-or-later
  */
